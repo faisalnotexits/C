c3c93fe2b4c260ccd60aef9192aafcbcd2c576c0
Faisal <ahmedfaisal3570@gmail.com>
2025-09-28 23:21:25 +0600

    Add files via upload

---
+++ AreaOfaCircle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter The radius :");
+    scanf("%f",&radius);
+
+   
+
+    printf("Area is : %f", 3.1416 * radius * radius);
+
+   return 0;
+
+}
