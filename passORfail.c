f2ab3e3bdb7d8427b0d7999d6f701544eee905d3
Faisal <ahmedfaisal3570@gmail.com>
2025-09-29 00:26:46 +0600

    Add files via upload

---
+++ passORfail.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int marks;
+    printf("Enter Student Marks :");
+    scanf("%d",&marks);
+
+    if( marks < 0 && marks <=30){
+        printf("fail\n");
+    }
+    else if( marks > 30 && marks <= 100){
+        printf("pass");
+    }
+    else {
+        printf("Invalid Marks");
+    }
+    return 0;
+
+}
