3e1a6ea176862e4c5fb04ffbf15a4bbc716fa833
Faisal <ahmedfaisal3570@gmail.com>
2025-09-29 00:26:28 +0600

    Add files via upload

---
+++ GradeToaStudent.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+
+    int marks;
+    printf("Enter Marks (0-100) :");
+    scanf("%d",&marks);
+
+    if ( marks < 30){
+
+        printf ("C\n");
+
+    }
+    else if ( marks >= 30 && marks < 70){
+
+        printf("A\n");
+
+    }
+    else if ( marks >= 70 && marks <=90){
+        printf("A\n");
+
+    }
+    else if ("marks > 90 && marks<= 100"){
+        printf("A+\n");
+
+    }
+    else{
+        printf("Invalid Number");
+
+    }
+    return 0;
+}
