cdd91affbc6a4e413300ed91dcc69a6ab23c7468
Faisal <ahmedfaisal3570@gmail.com>
2025-09-29 14:29:59 +0600

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+
+
+    int i=0;
+    while ( i <= n){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+
+
+
+
+
+
+
+}
+
