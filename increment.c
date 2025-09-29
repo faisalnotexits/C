391c38e485b73e2a5f8709fa87b995a91912d6c8
Faisal <ahmedfaisal3570@gmail.com>
2025-09-29 14:09:50 +0600

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    // increament operator
+    // ++i (pre-increment)
+    // i++ (post-increment)
+    
+    int i=1;
+    printf("%d\n",i++);  // use then increase
+    printf("%d\n", i);
+
+
+    printf("%d\n",++i);  // increase then use
+    printf("%d", i);
+
+    
+    return 0;
+}
